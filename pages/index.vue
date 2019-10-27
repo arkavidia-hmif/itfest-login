c56f4a3ac666927990cbb25c5d675ddd741ea48e
Myx-x <mkamadeus.mka@gmail.com>
2019-10-27 22:38:33 +0700

    First layout login

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-content>
+    <v-container fluid class="fill-height">
+      <v-row wrap justify="center" align="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-card raised p-4>
+            <v-row align="center" justify="center">
+              <v-col cols="10">
+                <v-img src="/logo_horizontal.svg" class="justify-center" />
+              </v-col>
+            </v-row>
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-title class="headline mb-1">
+                  Login
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  Silakan lakukan login terlebih dahulu.
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-card-text>
+              <v-form>
+                <v-text-field label="Username" type="text" />
+                <v-text-field label="Password" type="password" />
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
